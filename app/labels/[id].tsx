9469be2b3ee59e7039653cb57d18379486427019
nagatetsu08 <nagatetsu08@gmail.com>
2025-10-24 19:28:43 +0900

    ラベルピッカーUI作成

--- app/labels/[id].tsx
+++ app/labels/[id].tsx
@@ -1,6 +1,10 @@
 // ラベル修正画面
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { useRouter, useLocalSearchParams } from 'expo-router';
+import { Input, InputField, VStack, Button, ButtonText } from '@gluestack-ui/themed';
+import { useState } from 'react';
+import { ColorPicker } from '../../src/components/ColorPicker';
+
 
 export default function LabelEditScreen() {
   const router = useRouter();
@@ -12,20 +16,52 @@ export default function LabelEditScreen() {
   // const { userId } = route.params as { userId: number };
 
   const { id } = useLocalSearchParams();
+  const [labelName, setLabelName] = useState<string>("") // ラベル名
+  const [color, setColor] = useState<string | undefined>(undefined) // カラー
 
   // 作成ボタンを押下したらデータを保存して、画面自体は閉じる。
   // この画面はModalで開くので、dismiss()で閉じることが可能
-  const handleCreatePress = () => {
+  const handleEditPress = () => {
     router.dismiss();
   }
 
+  // 削除押下時の処理
+  const handleDeletePress = () => {
+    router.dismiss();
+  }
+
+  /**
+   * カラーピッカーで色が選択された際の処理
+   * @param color
+   *
+   */
+  const handleColorPress = (color: string) => {
+    setColor(color)
+  }
+
+
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>ラベル修正: {id}</Text>
-      <Button
-        title="修正"
-        onPress={handleCreatePress}
-      />
+      <VStack space="lg">
+
+        {/* ラベル名入力エリア */}
+        <Input variant="underlined" size="md" backgroundColor="$white" borderColor="$warmGray300">
+          <InputField paddingLeft={"$2"} placeholder='ラベル名' onChangeText={setLabelName}/>
+        </Input>
+
+        {/* カラーピッカー */}
+        <ColorPicker onPress={() => handleColorPress(color)} />
+
+        <VStack space="md">
+          <Button size="md" action="primary" marginHorizontal={"$4"} onPress={handleEditPress}>
+            <ButtonText>修正</ButtonText>
+          </Button>
+          <Button size="md" action="negative" marginHorizontal={"$4"} onPress={handleDeletePress}>
+            <ButtonText>削除</ButtonText>
+          </Button>
+        </VStack>
+      </VStack>
     </View>
   );
 }
@@ -34,11 +70,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#EFEFF4',
-    alignItems: 'center',
-    justifyContent: 'center'
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold'
-  }
 });
--- app/labels/create.tsx
+++ app/labels/create.tsx
@@ -1,9 +1,15 @@
 // ラベル作成画面
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { useRouter } from 'expo-router';
+import { StyleSheet, Text, View} from 'react-native';
+import { useRouter} from 'expo-router';
+import { Input, InputField, VStack, Button, ButtonText } from '@gluestack-ui/themed';
+import { useState } from 'react';
+import { ColorPicker } from '../../src/components/ColorPicker';
 
 export default function LabelCreateScreen() {
   const router = useRouter();
+
+  const [labelName, setLabelName] = useState<string>("")            // ラベル名
+  const [color, setColor] = useState<string | undefined>(undefined) // カラー
 
   // 作成ボタンを押下したらデータを保存して、画面自体は閉じる。
   // この画面はModalで開くので、dismiss()で閉じることが可能
@@ -11,13 +17,32 @@ export default function LabelCreateScreen() {
     router.dismiss();
   }
 
+  /**
+   * カラーピッカーで色が選択された際の処理
+   * @param color
+   *
+   */
+  const handleColorPress = (color: string) => {
+    setColor(color)
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>ラベル作成</Text>
-      <Button
-        title="作成"
-        onPress={handleCreatePress}
-      />
+      {/* Vstackはコンポーネントをまとめて管理できるもの。（コンポーネントごとに一定間隔余白を与えたいときなど便利） */}
+      <VStack space="lg">
+        {/* ラベル名入力エリア */}
+        <Input variant="underlined" size="md" backgroundColor="$white" borderColor="$warmGray300">
+          <InputField paddingLeft={"$2"} placeholder='ラベル名' onChangeText={setLabelName}/>
+        </Input>
+
+        {/* カラーピッカー */}
+        <ColorPicker onPress={() => handleColorPress(color)} />
+
+        {/* actionはボタンの色 */}
+        <Button size="md" action="primary" marginHorizontal={"$4"} onPress={handleCreatePress}>
+          <ButtonText>作成</ButtonText>
+        </Button>
+      </VStack>
     </View>
   );
 }
@@ -26,11 +51,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#EFEFF4',
-    alignItems: 'center',
-    justifyContent: 'center'
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold'
-  }
 });
